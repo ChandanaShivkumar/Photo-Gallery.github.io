cc616ba9a8413fd5baf0d6477db618750b39a4dd
Chandana Shivkumar <chandanamshivkumar@gmail.com>
2021-11-04 19:07:41 +0530

    Photo Gallery

---
+++ custom.js
@@ -0,0 +1,70 @@
+
+var photo1 = document.getElementById('photo1');
+var photo1Src = document.getElementById('photo1').src;
+
+var photo2 = document.getElementById('photo2');
+var photo2Src = document.getElementById('photo2').src;
+
+var photo3 = document.getElementById('photo3');
+var photo3Src = document.getElementById('photo3').src;
+
+var photo4 = document.getElementById('photo4');
+var photo4Src = document.getElementById('photo4').src;
+
+var photo5 = document.getElementById('photo5');
+var photo5Src = document.getElementById('photo5').src;
+
+var photo6 = document.getElementById('photo6');
+var photo6Src = document.getElementById('photo6').src;
+
+var photo7 = document.getElementById('photo7');
+var photo7Src = document.getElementById('photo7').src;
+
+var photo8 = document.getElementById('photo8');
+var photo8Src = document.getElementById('photo8').src;
+
+var photo9 = document.getElementById('photo9');
+var photo9Src = document.getElementById('photo9').src;
+
+var photo10 = document.getElementById('photo10');
+var photo10Src = document.getElementById('photo10').src;
+
+photo1.addEventListener("click", function(){
+	main_image.src = photo1Src
+})
+
+photo2.addEventListener("click", function(){
+	document.getElementById('main_image').src = photo2Src
+})
+
+photo3.addEventListener("click", function(){
+	main_image.src = photo3Src
+})
+
+photo4.addEventListener("click", function(){
+	main_image.src = photo4Src
+})
+
+photo5.addEventListener("click", function(){
+	main_image.src = photo5Src
+})
+
+photo6.addEventListener("click", function(){
+	main_image.src = photo6Src
+})
+
+photo7.addEventListener("click", function(){
+	main_image.src = photo7Src
+})
+
+photo8.addEventListener("click", function(){
+	main_image.src = photo8Src
+})
+
+photo9.addEventListener("click", function(){
+	main_image.src = photo9Src
+})
+
+photo10.addEventListener("click", function(){
+	main_image.src = photo10Src
+})
